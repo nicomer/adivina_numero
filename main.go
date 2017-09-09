2c1953e968b255eff0dc3b1e36e922fae833abbf
nicomer <nicomercadogarcia@gmail.com>
2017-09-09 14:43:40 +0200

    mensaje de bienvenida

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Bienvenido al juego de las adivinanzas\n")
+	fmt.Printf("Debes adivinar un numero del 1 al 100\n")
+
+	// Obtener numero aleatorio
+	// Preguntar a usuario un número
+	// Comparar numero aleatorio con numero de usuario
+	// Si ha acertado salir del programa
+	// Si no ha acertado indicar si el numero es mayor o menor
+	// Volver a preguntar
+}
